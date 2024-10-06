84e8fc331ed571e9f0c7c3be3131a36071a0dff6
LaDaLo <lordorochiz@gmail.com>
2024-10-06 16:57:54 +0700

    Restructure

---
+++ LKHOANVI.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <assert.h>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <math.h>
+
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+const int mod = 1000000007;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST_IO;
+    // --------------- Solve ---------------  \\ 
+    
+    
+    ////////////////////////////////////////////
+    return 0;
+}
+
---
+++ gen.bat
@@ -0,0 +1,1 @@
+python Create_New_Problem.py %1 %2
